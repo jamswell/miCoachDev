90c938abf4a285ff43302cc1ce8d04a97dfd3ec5
Lukas <gugugs@x300-lukas.fritz.box>
2013-09-12 10:52:11 +0200

    added binary folder

---
+++ binarys/micoach-backup/libmicoach/errors.py
@@ -0,0 +1,12 @@
+
+class LoginFailed(Exception):
+    pass
+
+class NotAuthenticated(Exception):
+    pass
+
+class SessionTimeout(Exception):
+    pass
+
+class MissingCredential(Exception):
+    pass
---
+++ binarys/micoach-backup/libmicoach/user.py
@@ -0,0 +1,38 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+from libmicoach.services import *
+from libmicoach.schedule import *
+from libmicoach.errors import *
+
+class miCoachUser(object):
+    def __init__(self, email=None, password=None):
+        log.debug('Starting initializing miCoach user')
+        if not email is None and not password is None:
+            self.login(email, password)
+
+
+    def login(self, email, password):
+        self.profile = UserProfile(email, password)
+        self.schedule = Schedule()
+        
+        self.getProfile()
+        
+    def getProfile(self):
+        log.debug('Retrieving user informations')
+        
+        profile = self.profile.GetUserProfile()
+        self.screenName = str(profile.ScreenName)
+        self.email = str(profile.Email)
+
+    def logout(self):
+        pass
+        
+    def testConnection(self):
+        pass
+        
+    def getSchedule(self):
+        return self.schedule
+    
+    
